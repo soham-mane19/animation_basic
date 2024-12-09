ff7fc65d15daaedad7d2e87e2ffe71b507ae8ac9
soham-mane19 <fluttersohammane@gmail.com>
2024-12-10 00:47:24 +0530

    done the loading_animation in which cover animationbuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:animation_starter_code/explicit_examples/list_animation.dart';
+import 'package:animation_starter_code/explicit_examples/loading_animation.dart';
 import 'package:animation_starter_code/explicit_examples/login_animation.dart';
 import 'package:flutter/material.dart';
 
@@ -17,7 +18,10 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ListAnimation(),
+      home: const RadialProgressAnimation(
+        progress: 0.7,
+        color: Colors.blue,
+      ),
     );
   }
 }
